b4438680c78e44ad2ad98b07dbde0b5bfe61acbc
zhongwu <zhongwuzw@qq.com>
2016-06-13 15:08:27 +0800

    App测试次数统计
    App测试次数统计

--- hefen_python/hefen.py
+++ hefen_python/hefen.py
@@ -13,11 +13,44 @@ def datetime_timestamp(dt):
     s = time.mktime(time.strptime(dt, '%Y-%m-%d %H:%M:%S'))
     return int(s)
 
+
+def isAppFeatureComplete(planid, case_type_name):
+    connection = pymysql.connect(host='192.168.16.113', port=3306, user='zhangbin', password='zhangbin', db='terminal',
+                                 charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
+    try:
+        with connection.cursor() as cursor:
+            sql = "select test_status from upload_plan_case WHERE planid = '" + planid + "' and case_type_name = '" + case_type_name + "'"
+            cursor.execute(sql)
+            result = cursor.fetchone()
+            if result["test_status"] == "103":
+                return True
+            else:
+                return False
+    finally:
+        connection.close()
+
+
+def getAppTestData(planid):
+    internetDownload = isAppFeatureComplete(planid, u"互联网下载测试")
+    videoTencentNum = isAppFeatureComplete(planid, u"视频测试-腾讯电影")
+    videoIQiyiNum = isAppFeatureComplete(planid, u"视频测试-爱奇艺电视剧")
+    webBrowsing = isAppFeatureComplete(planid, u"网页浏览测试")
+
+    videoTestNum = False
+    if videoIQiyiNum and videoTencentNum:
+        videoTestNum = True
+
+    totalTestNum = False
+    if videoTestNum and internetDownload and webBrowsing:
+        totalTestNum = True
+
+    return (totalTestNum, internetDownload, videoTestNum, webBrowsing)
+
+
 connection = pymysql.connect(host='192.168.16.113',port=3306,user='zhangbin',password='zhangbin',db='terminal',charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
 
 try:
     with connection.cursor() as cursor:
-        # Read a single record
         addr = u'北京市西城区西便门内大街甲53号'
         d = datetime_timestamp('2016-05-19 00:00:00')
         d1 = datetime_timestamp('2016-05-20 00:00:00')
@@ -31,10 +64,19 @@ try:
 
         sql = "select * from upload_plan WHERE client_number = '11111111111' AND addr = '" + addr + "' and UNIX_TIMESTAMP(creattime) > " + str(d) + " and UNIX_TIMESTAMP(creattime) < " + str(d1)
         cursor.execute(sql)
-        result = cursor.fetchmany(30)
-        print(result)
+        result = cursor.fetchall()
+
+        appTestResult = [0]*4
+        for element in result:
+            data = getAppTestData(element["planid"])
+            for i in range(4):
+                appTestResult[i] += data[i]
+
+        print  appTestResult
+
+
 finally:
     connection.close()
 
-n = os.system('/Users/zhongwu/Documents/workspace/test.sh 2014')
-print n
+# n = os.system('/Users/zhongwu/Documents/workspace/test.sh 2014')
+# print n
