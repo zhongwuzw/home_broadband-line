e8d05219ca8e6598c5572636ecce77cd3481ace9
zhongwuzw <zhongwuzw@qq.com>
2016-10-20 17:50:08 +0800

    取消数据同步，直接在原库中处理
    取消数据同步，直接在原库中处理

--- hefen_python/test/py_test.py
+++ hefen_python/test/py_test.py
@@ -10,21 +10,4 @@ time_converted = time_osx.strftime("%a, %d %b %Y %H:%M:%S")
 
 print time_converted
 
-n = os.system('/Users/zhongwu/Documents/workspace/test.sh 2014')
-print n
 
-src_pc_database = pymysql.connect(host='192.168.39.51', port=5151, user='zhongwu', password='zhongwu',
-                               db='testdataanalyse',
-                               charset='utf8mb4', cursorclass=pymysql.cursors.SSDictCursor)
-
-try:
-    with src_pc_database.cursor() as src_cursor:
-        sql = "select * from t_user"
-        src_cursor.execute(sql)
-        result = src_cursor.fetchone()
-        while result is not None:
-            print result[0]
-            result = src_cursor.fetchone()
-finally:
-    print
-src_pc_database.close()
