42efa2fe644c9f9be21e2d95fe5a4c8357e91f8d
zhongwuzw <zhongwuzw@qq.com>
2016-10-21 16:21:59 +0800

    调整脚本间隔时间，修复跳转链接bug
    调整脚本间隔时间，修复跳转链接bug

--- hefen_python/jiakuan/routine_not_data_sync.py
+++ hefen_python/jiakuan/routine_not_data_sync.py
@@ -11,18 +11,20 @@ yesterday_month = time.strftime("%Y%m", yesterday.timetuple())
 
 #执行中间表脚本
 os.system('/opt/Script/jiakuandata_online/total.sh ' + yesterday_month)
-time.sleep(4 * 60 * 60)
+time.sleep(2 * 60 * 60)
 
 print "执行完中间表脚本"
 
 #执行结果脚本
 os.system('/opt/Script/jiakuandata_online/jiakuandata/jiakuandata.sh ' + yesterday_month)
 
+print "执行"
+
 #替换累计用户数指标配置的日期
 os.system('sed -i "s#^testtime=.*#testtime=' + yesterday_month + '#g" /home/probeStatistical_online/res/conf/monitor.ini')
 
 #执行累计用户数指标脚本
-time.sleep(4 * 60 * 60)
+time.sleep(2 * 60 * 60)
 
 print "执行完结果脚本"
 
